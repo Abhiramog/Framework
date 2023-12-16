8cb7cc7bed33a4cc585e972dfa3b998f79f11006
BadriSivaSai <badriprabha007@gmail.com>
2023-12-16 11:09:24 +0530

    one more statement has added

--- src/test/java/com/Example1/Class1.java
+++ src/test/java/com/Example1/Class1.java
@@ -5,6 +5,7 @@ public class Class1 {
 	public static void main(String[] args) {
 	System.out.println("hi this is badri modification class");
 	System.out.println(" hi hello gitbub");
+	System.out.println("wdjwidjijkj");
 	}
 
 }
